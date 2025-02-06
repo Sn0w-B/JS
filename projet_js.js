6fe0ea742743548562d686983f8fb77b000949ba
GregBlanzat <gregoire.blanzat@gmail.com>
2025-02-06 10:38:57 +0100

    Update projet_js.js

--- projet_js.js
+++ projet_js.js
@@ -1,2 +1,12 @@
-console.log('Hello');
-alert("coucou");
+
+function melanger_liste(Liste) {
+    return Liste.sort(() => Math.random() - 0.5);
+}
+
+let Liste_mots = ["Boîte", "Chapeau", "Danse"];
+let listeMelangee = melanger_liste(Liste_mots);
+
+let affiche_elem = function(elem) {
+    console.log(elem);
+}
+let Mot_actif = listeMelangee[0] //Mot de la manche (qui changera 13 fois)
